8277470c6c257ec8e376233d70b41db68933a081
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-08-22 03:01:42 -0700

    final comit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author vaishnavichaurasia
  * @version 1.0
  * */
 public class Main {
     public static void main(String[] args) {
-        String username = "";
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine().trim();
+        System.out.println("Welcome" + username + "!");
+        System.out.println("Ho gya");
         scanner.close();
-
     }
 }
