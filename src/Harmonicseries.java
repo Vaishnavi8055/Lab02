7328a8a83022803ac5ff865bda59e8652afae2c8
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-08-29 02:21:11 -0700

    sum as output

--- src/Harmonicseries.java
+++ src/Harmonicseries.java
@@ -5,6 +5,12 @@ public class Harmonicseries {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter  the value of n");
         int n = sc.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + (1.0 / i);
+        }
+        System.out.println("sum is " + sum);
+
 
 
     }
