cc17df46d782595ce3aa90cc1c1b84ecb94d5fb6
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-08-29 02:47:26 -0700

    enter a temp

--- src/Rupeespaisa.java
+++ src/Rupeespaisa.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
+
 public class Rupeespaisa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number in float");
         double n = sc.nextDouble();
+        System.out.println("Enter the temperature in celsius");
+
 
     }
 
