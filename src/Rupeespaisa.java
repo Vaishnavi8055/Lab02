17579660cc8040ae0f460ff5114c035cdb3f4d27
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-08-29 02:32:07 -0700

    input from user

---
+++ src/Rupeespaisa.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Rupeespaisa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number in float");
+        double n = sc.nextDouble();
+
+    }
+
+}
